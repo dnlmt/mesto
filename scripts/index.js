d813b1eaf93d8c30052aca1943b143c6523aeeec
dnlmt <daneelmt@gmail.com>
2023-06-29 23:34:55 +0600

    попытка намбер 2

--- scripts/index.js
+++ scripts/index.js
@@ -2,23 +2,23 @@ let popup = document.querySelector('.popup');
 let editButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popup__close-button');
 
+let formElement = document.querySelector('.popup__form');
+
+let nameInput = formElement.querySelector('#userName');
+let jobInput = formElement.querySelector('#userSpec');
+
+let profileName = document.querySelector('.profile__name');
+let profileSpeciality = document.querySelector('.profile__speciality');
+
 function openPopup() {
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileSpeciality.textContent;
     popup.classList.add('popup_opened');
 }
 
 function closePopup() {
     popup.classList.remove('popup_opened');
 }
-
-editButton.addEventListener('click', openPopup);
-closeButton.addEventListener('click', closePopup);
-
-
-// Находим форму в DOM
-let formElement = document.querySelector('.popup');
-// Находим поля формы в DOM
-let nameInput = formElement.querySelector('#userName');
-let jobInput = formElement.querySelector('#userSpec');
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
@@ -27,23 +27,17 @@ function handleFormSubmit (evt) {
                                                 // Так мы можем определить свою логику отправки.
                                                 // О том, как это делать, расскажем позже.
 
-    // Получите значение полей jobInput и nameInput из свойства value
-
     let userNameInput = nameInput.value;
     let userJobInput = jobInput.value;
-
-    // Выберите элементы, куда должны быть вставлены значения полей
-
-    let profileName = document.querySelector('.profile__name');
-    let profileSpeciality = document.querySelector('.profile__speciality');
-
-    // Вставьте новые значения с помощью textContent
 
     profileName.textContent = userNameInput;
     profileSpeciality.textContent = userJobInput;
     
     closePopup();
 }
+
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»

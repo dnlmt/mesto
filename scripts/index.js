8c6ba5c93b436d3e06afc2b76b8cd3f788acbbf1
dnlmt <daneelmt@gmail.com>
2023-07-10 22:49:28 +0600

    functional cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,31 @@
-let popup = document.querySelector('.popup');
-let editButton = document.querySelector('.profile__edit-button');
-let closeButton = document.querySelector('.popup__close-button');
+const popup = document.querySelector('.popup');
+const profilePopup = document.querySelector('#profile');
+const addCardPopup = document.querySelector('#addCard');
+const editButton = document.querySelector('.profile__edit-button');
+const addButton = document.querySelector('.profile__add-button');
+const profilePopupCloseButton = document.querySelector('#profilePopupCloseButton');
+const addCardPopupCloseButton = document.querySelector('#addCardPopupCloseButton');
+const createButton = document.querySelector('#createCard');
 
-let formElement = document.querySelector('.popup__form');
+const imagePopup = document.querySelector('#showImage');
+const closeImageButton = document.querySelector('#closeImage');
 
-let nameInput = formElement.querySelector('#userName');
-let jobInput = formElement.querySelector('#userSpec');
+const formElement = document.querySelector('.popup__form');
+const addCardFormElement = document.querySelector('#addCardForm');
 
-let profileName = document.querySelector('.profile__name');
-let profileSpeciality = document.querySelector('.profile__speciality');
+const nameInput = formElement.querySelector('#userName');
+const jobInput = formElement.querySelector('#userSpec');
 
-  const elements = document.querySelector('.elements');
-  const template = document.querySelector('#element-template').content;
+const placeInput = addCardFormElement.querySelector('#name');
+const imageInput = addCardFormElement.querySelector('#link');
 
-  const renderElements = (element) => {
+const profileName = document.querySelector('.profile__name');
+const profileSpeciality = document.querySelector('.profile__speciality');
+
+const elements = document.querySelector('.elements');
+const template = document.querySelector('#element-template').content;
+
+const renderElements = (element) => {
     const card = template.querySelector('.element').cloneNode(true);
     const place = card.querySelector('.element__place');
     const image = card.querySelector('.element__photo');
@@ -36,6 +48,18 @@ let profileSpeciality = document.querySelector('.profile__speciality');
         card.remove();
     })
 
+    function showImage() {
+        const img = document.querySelector('.popup__image');
+        const name = document.querySelector('.popup__image-name')
+    
+        img.src = element.link;
+        img.alt = element.name;
+        name.textContent = element.name;
+        imagePopup.classList.add('popup_opened');
+    }
+
+    image.addEventListener('click', showImage);
+
     elements.append(card);
   } 
 
@@ -43,14 +67,26 @@ let profileSpeciality = document.querySelector('.profile__speciality');
     renderElements(card);
   });
 
-function openPopup() {
+function openProfilePopup() {
     nameInput.value = profileName.textContent;
     jobInput.value = profileSpeciality.textContent;
-    popup.classList.add('popup_opened');
+    profilePopup.classList.add('popup_opened');
 }
 
-function closePopup() {
-    popup.classList.remove('popup_opened');
+function closeProfilePopup() {
+    profilePopup.classList.remove('popup_opened');
+}
+
+function openAddCardPopup() {
+    addCardPopup.classList.add('popup_opened');
+}
+
+function closeAddCardPopup() {
+    addCardPopup.classList.remove('popup_opened');
+}
+
+function closeImagePopup() {
+    imagePopup.classList.remove('popup_opened');
 }
 
 // Обработчик «отправки» формы, хотя пока
@@ -66,12 +102,34 @@ function handleFormSubmit (evt) {
     profileName.textContent = userNameInput;
     profileSpeciality.textContent = userJobInput;
     
-    closePopup();
+    closeProfilePopup();
 }
 
-editButton.addEventListener('click', openPopup);
-closeButton.addEventListener('click', closePopup);
+function addCard (evt) {
+    evt.preventDefault();
+
+    const newCard = 
+        {
+        name: placeInput.value,
+        link: imageInput.value
+    }
+
+    renderElements(newCard);
+
+    closeAddCardPopup();
+}
+
+editButton.addEventListener('click', openProfilePopup);
+addButton.addEventListener('click', openAddCardPopup)
+profilePopupCloseButton.addEventListener('click', closeProfilePopup);
+addCardPopupCloseButton.addEventListener('click', closeAddCardPopup);
+
+closeImageButton.addEventListener('click', closeImagePopup);
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', handleFormSubmit); 
+
+createCard.addEventListener('click', addCard);
+
+

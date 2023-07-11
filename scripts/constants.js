c39e5847d2284c3c0271ec0b6b892b9573ef5e7b
dnlmt <daneelmt@gmail.com>
2023-07-11 22:49:38 +0600

    fix

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,15 +1,15 @@
-const profilePopup = document.querySelector('.popup__profile');
-const addCardPopup = document.querySelector('.popup__add-card');
+const profilePopup = document.querySelector('.popup_profile');
+const addCardPopup = document.querySelector('.popup_add-card');
 const editButton = document.querySelector('.profile__edit-button');
 const addButton = document.querySelector('.profile__add-button');
-const profilePopupCloseButton = document.querySelector('.popup__profile_close-button');
-const addCardPopupCloseButton = document.querySelector('.popup__add-card_close-button');
+const profilePopupCloseButton = document.querySelector('.popup_profile_close-button');
+const addCardPopupCloseButton = document.querySelector('.popup_add-card_close-button');
 
-const imagePopup = document.querySelector('.popup__show-image');
-const closeImageButton = document.querySelector('.button__image_close-button');
+const imagePopup = document.querySelector('.popup_show-image');
+const closeImageButton = document.querySelector('.button_image_close-button');
 
-const profilePopupFormElement = document.querySelector('.popup__profile_form');
-const addCardFormElement = document.querySelector('.popup__add-card_form');
+const profilePopupFormElement = document.querySelector('.popup_profile_form');
+const addCardFormElement = document.querySelector('.popup_add-card_form');
 
 const nameInput = profilePopupFormElement.querySelector('#userName');
 const jobInput = profilePopupFormElement.querySelector('#userSpec');
